3be6b5bdec0745c6a2b21fb276a989c64a5eec7e
Paul Seyerlein <paul.seyerlein@gmail.com>
2022-12-21 10:51:35 +0100

    basic strcuture for third puzzle

--- src/main/java/seyerlein/third/RucksackRearranger.java
+++ src/main/java/seyerlein/third/RucksackRearranger.java
@@ -1,5 +1,11 @@
 package seyerlein.third;
 
+import java.util.List;
+
 public class RucksackRearranger
 {
+    public int processInput(List<String> inputLines)
+    {
+        return 0;
+    }
 }
--- src/test/java/seyerlein/third/RucksackRearrangerTest.java
+++ src/test/java/seyerlein/third/RucksackRearrangerTest.java
@@ -1,12 +1,22 @@
 package seyerlein.third;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import seyerlein.ListStringResource;
+import seyerlein.ListStringResourceExtension;
 
+import java.util.List;
+
+@ExtendWith(ListStringResourceExtension.class)
 class RucksackRearrangerTest
 {
-@Test
-    void test(){
-    char c = 'a';
-    System.out.println((int)c);
-}
+    private static final String INPUT_FILE = "/third/input.txt";
+
+    @Test
+    void test(@ListStringResource(INPUT_FILE) List<String> inputLines)
+    {
+        RucksackRearranger rearranger = new RucksackRearranger();
+        Assertions.assertThat(rearranger.processInput(inputLines)).isEqualTo(1);
+    }
 }
