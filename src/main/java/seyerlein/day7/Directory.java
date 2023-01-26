1c647805d5a22864305938b10a3b61801169151f
Paul Seyerlein <paul.seyerlein@gmail.com>
2023-01-26 11:13:53 +0100

    implemented solution for sample input for puzzle 7

--- src/main/java/seyerlein/day7/Directory.java
+++ src/main/java/seyerlein/day7/Directory.java
@@ -1,5 +1,29 @@
 package seyerlein.day7;
 
-public record Directory(int size)
+import java.util.Optional;
+
+public class Directory
 {
+    private final Directory parent;
+    private int size = 0;
+
+    public Directory(Directory parent)
+    {
+        this.parent = parent;
+    }
+
+    public Optional<Directory> getParent()
+    {
+        return Optional.ofNullable(parent);
+    }
+
+    public int getSize()
+    {
+        return size;
+    }
+
+    public void incrementSize(int amount)
+    {
+        this.size += amount;
+    }
 }
--- src/main/java/seyerlein/day7/FileSystemCalculator.java
+++ src/main/java/seyerlein/day7/FileSystemCalculator.java
@@ -2,38 +2,65 @@ package seyerlein.day7;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class FileSystemCalculator
 {
     private static final int THRESHOLD = 100000;
+    private static final Pattern FILE_SIZE_PATTERN = Pattern.compile("(\\d+)\s");
 
     public int getSumOfDirectories(List<String> inputLines)
     {
-        //parse commands into datastructure(?)
+        //parse commands into datastructure
         List<Directory> dirs = this.parseInput(inputLines);
         //find all dirs with specific file size
         //sum all dirs
         return dirs.stream()
-                .map(Directory::size)
+                .map(Directory::getSize)
                 .filter(size -> size <= THRESHOLD)
                 .reduce(0, Integer::sum);
     }
 
     private List<Directory> parseInput(List<String> inputLines)
     {
-        List<Directory> dirs = new ArrayList<>();
-        int depth = 0;
+        List<Directory> result = new ArrayList<>();
+        Directory currentDir = null;
         for (String line : inputLines)
         {
-            if (line.startsWith("$ cd"))
+            Matcher matcher = FILE_SIZE_PATTERN.matcher(line);
+
+            if (line.startsWith("$ cd ..") && currentDir != null)
             {
+                result.add(currentDir);
+                currentDir = currentDir.getParent().orElse(null);
+            }
+            else if (line.startsWith("$ cd"))
+            {
+                Directory dir = new Directory(currentDir);
+                //save root directory to result
+                if (currentDir == null)
+                {
+                    result.add(dir);
+                }
                 //change pointer
+                currentDir = dir;
             }
-            else if (line.startsWith("$ ls"))
+            else if (matcher.find())
             {
-                //read until next $
+                //file size line
+                int fileSize = Integer.parseInt(matcher.group(1));
+                Directory directoryPointer = currentDir;
+                while (directoryPointer != null)
+                {
+                    directoryPointer.incrementSize(fileSize);
+                    directoryPointer = directoryPointer.getParent().orElse(null);
+                }
             }
+            //ignore ls and dir lines completely
         }
-        return dirs;
+        //must add the last directory to the result as well
+        result.add(currentDir);
+        return result;
     }
 }
